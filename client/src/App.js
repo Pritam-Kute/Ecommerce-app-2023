d6c55e5c90eec670ffdff23d2880e301f598bffe
Pritam.Kute <pritamkute1171@gmail.com>
2023-11-11 17:13:31 +0530

    add new feature - Search category and Similar filter wise

--- client/src/App.js
+++ client/src/App.js
@@ -18,30 +18,32 @@ import Orders from "./pages/user/Orders.js";
 import Profile from "./pages/user/Profile.js";
 import Products from "./pages/Admin/Products.js";
 import UpdateProduct from "./pages/Admin/UpdateProduct.js";
+import Search from "./pages/Search.js";
+import ProductDetails from "./pages/ProductDetails.js";
+import Categories from "./pages/Categories.js";
+import CategoryProduct from "./pages/CategoryProduct.js";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:slug" element={<CategoryProduct />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
           <Route path="user/orders" element={<Orders />} />
           <Route path="user/profile" element={<Profile />} />
         </Route>
         <Route path="/dashboard" element={<AdminRoute />}>
-          <Route path="admin" element={<AdminDashboard />}></Route>
-          <Route
-            path="admin/create-category"
-            element={<CreateCategory />}
-          ></Route>
-          <Route
-            path="admin/create-product"
-            element={<CreateProduct />}
-          ></Route>
-          <Route path="admin/product/:slug" element={<UpdateProduct />}></Route>
-          <Route path="admin/products" element={<Products />}></Route>
-          <Route path="admin/users" element={<Users />}></Route>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Products />} />
+          <Route path="admin/users" element={<Users />} />
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
